a569c45089ca84ab58b5b36eb46e5d410aa3f86c
Ruhul Amin <ruhulaminbabu93@gmail.com>
2024-12-26 00:18:49 +0600

    Implement addFood api

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.joj1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,27 +25,72 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
     // Collection Name
-    const foodsCollection = client.db('foodhive').collection('foods')
-    const purchasesCollection = client.db('foodhive').collection('purchases');
+    const foodsCollection = client.db("foodhive").collection("foods");
+    const purchasesCollection = client.db("foodhive").collection("purchases");
 
-    // Get all foods APIs 
-    app.get('/foods', async(req, res) => {
-        const cursor = foodsCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    // Get all foods APIs
+    app.get("/foods", async (req, res) => {
+      const cursor = foodsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Get a single food item by ID
-    app.get('/foods/:id', async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await foodsCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // add food item
+    app.post("/addFood", async (req, res) => {
+      const {
+        name,
+        category,
+        image,
+        price,
+        quantity,
+        description,
+        shortDescription,
+        purchaseCount,
+        foodOrigin,
+        addedBy,
+      } = req.body;
+      const newFood = {
+        name,
+        category,
+        image,
+        price: parseFloat(price),
+        quantity: parseInt(quantity, 10),
+        description,
+        shortDescription,
+        purchaseCount: purchaseCount || 0,
+        foodOrigin,
+        addedBy: {
+          name: addedBy.name,
+          email: addedBy.email,
+        },
+      };
+      const result = await foodsCollection.insertOne(newFood);
+      res.send(result);
+    });
+
     // Purchase a food item with update purchaseCount
-    app.post('/purchaseFood', async(req, res) => {
-      const { foodId, foodName, buyerName, buyerEmail, quantity, price, buyingDate,foodImg } = req.body
+    app.post("/purchaseFood", async (req, res) => {
+      const {
+        foodId,
+        foodName,
+        buyerName,
+        buyerEmail,
+        quantity,
+        price,
+        buyingDate,
+        foodImg,
+      } = req.body;
       const purchase = {
         foodId: new ObjectId(foodId),
         foodName,
@@ -57,35 +101,34 @@ async function run() {
         buyingDate,
         foodImg,
       };
-      const result =await purchasesCollection.insertOne(purchase)
+      const result = await purchasesCollection.insertOne(purchase);
       // update purchaseCount
       const updateResult = await foodsCollection.updateOne(
-        {_id: new ObjectId(foodId)},
-        {$inc: {purchaseCount: 1}}
+        { _id: new ObjectId(foodId) },
+        { $inc: { purchaseCount: 1 } }
       );
       if (result.acknowledged && updateResult.modifiedCount > 0) {
-        res.status(200).send({ message: 'Purchase successful' });
+        res.status(200).send({ message: "Purchase successful" });
       } else {
-        res.status(500).send({ message: 'Error occurred during purchase' });
+        res.status(500).send({ message: "Error occurred during purchase" });
       }
-    })
+    });
 
-    // Get order by user 
-    app.get('/myOrders/:email', async(req, res) => {
+    // Get order by user
+    app.get("/myOrders/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {buyerEmail: email};
-      const orders = await purchasesCollection.find(query).toArray()
-      res.send(orders)
-    })
+      const query = { buyerEmail: email };
+      const orders = await purchasesCollection.find(query).toArray();
+      res.send(orders);
+    });
 
-    // Delete a specific order 
-    app.delete('/deleteOrder/:orderId', async(req, res) => {
+    // Delete a specific order
+    app.delete("/deleteOrder/:orderId", async (req, res) => {
       const { orderId } = req.params;
       const query = { _id: new ObjectId(orderId) };
       const result = await purchasesCollection.deleteOne(query);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,11 +136,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('This is FoodHive server')
-})
+app.get("/", (req, res) => {
+  res.send("This is FoodHive server");
+});
 
 app.listen(port, () => {
-    console.log(`FoodHive server is running at : ${port}`)
-})
+  console.log(`FoodHive server is running at : ${port}`);
+});
